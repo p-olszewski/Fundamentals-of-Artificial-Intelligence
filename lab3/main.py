d69accdcadfb6a148a9b35d6971deec6d2b969f2
p-olszewski <przemo1297@gmail.com>
2023-01-26 20:45:02 +0100

    LAB3 add train function

--- lab3/main.py
+++ lab3/main.py
@@ -10,6 +10,32 @@ def fa(weights, inputs):
     return sigmoid(np.dot(inputs, weights))
 
 
+def train():
+    w = np.array([[0.0629, -0.0805, -0.0685],
+                  [0.0812, -0.0443, 0.0941],
+                  [-0.0746, 0.0094, 0.0914],
+                  [0.0827, 0.0915, -0.0029],
+                  [0.0265, 0.0930, 0.0601]])
+    p = np.array([[4, 2, -1],
+                  [0.01, -1, 3.5],
+                  [0.01, 2, 0.01],
+                  [-1, 2.5, -2],
+                  [-1.5, 2, 1.5]])
+    t = np.array([[1, 0, 0],
+                  [0, 1, 0],
+                  [0, 0, 1]])
+
+    for i in range(10):
+        nr_example = random.randint(0, 2)
+        x = p[:, nr_example]
+        y = fa(w, x)
+        d = t[:, nr_example] - y
+        x_reshape = x.reshape(5, 1)
+        d_reshape = d.reshape(3, 1)
+        d_w = 0.1 * np.dot(x_reshape, d_reshape.T)
+        w += d_w
+    return w
+
 
 if __name__ == '__main__':
     sigmoid()
