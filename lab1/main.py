a2fde9e6c0a28dc416ec5157b89618a0c1692074
p-olszewski <przemo1297@gmail.com>
2023-01-26 10:19:40 +0100

    Add comments to other functions

--- lab1/main.py
+++ lab1/main.py
@@ -24,12 +24,15 @@ def cities_init():
 # Method 1
 def brute_force_method(points):
     n = len(points)
-    min_dist = float('inf')
+    min_dist = float('inf')  # infinity
     best_path = None
 
+    # iterate through all possible paths
     for path in permutations(points):
         dist = 0
+        # calculate the distance of the current path
         for i in range(n - 1):
+            # add distance between last city and first city
             dist += euclidean_distance(path[i], path[i + 1])
         dist += euclidean_distance(path[-1], path[0])
         if dist < min_dist:
@@ -46,6 +49,7 @@ def n_n_method(points):
     remaining_points = points[1:]
 
     while remaining_points:
+        # nearest point to the current city
         nearest = min(remaining_points, key=lambda x: euclidean_distance(path[-1], x))
         path.append(nearest)
         remaining_points.remove(nearest)
@@ -53,24 +57,25 @@ def n_n_method(points):
     total_distance = sum(euclidean_distance(path[i], path[i + 1])
                          for i in range(len(path) - 1)) + euclidean_distance(path[-1], path[0])
 
-    print("\nN-N method path: " + str(path) + "\nN-N method distance: " + str(round(total_distance, 2)))
+    print("\nN-N method path: " + str(path) +
+          "\nN-N method distance: " + str(round(total_distance, 2)))
 
 
 # Method 3 (instead of Dijkstra's algorithm )
 def rhc_method(cities):
     n = len(cities)
-    # generate initial random path
+    # initial random path
     best_path = random.sample(range(n), n)
-    # calculate the length of the initial path
+    # length of the initial path
     best_path_length = sum(euclidean_distance(cities[best_path[i]], cities[best_path[i + 1]])
                            for i in range(len(best_path) - 1)) + euclidean_distance(cities[best_path[-1]],
                                                                                     cities[best_path[0]])
 
     while True:
         neighbour_path = best_path.copy()
-        l1, l2 = random.sample(range(n), 2)  # select two random indices to swap
+        l1, l2 = random.sample(range(n), 2)  # two random indices to swap
         neighbour_path[l1], neighbour_path[l2] = neighbour_path[l2], neighbour_path[l1]  # swap the indices in the copy
-        # calculate the length of the new path
+        # length of the new path
         neighbour_path_length = sum(euclidean_distance(cities[neighbour_path[i]], cities[neighbour_path[i + 1]])
                                     for i in range(len(neighbour_path) - 1)) + euclidean_distance(
             cities[neighbour_path[-1]],
