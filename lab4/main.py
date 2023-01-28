cae8eae0927a63eba4449cafc934c205e7c4427f
p-olszewski <przemo1297@gmail.com>
2023-01-29 00:53:13 +0100

    LAB4 loop hidden_layer_neurons, display plots, code cleanup

--- lab4/main.py
+++ lab4/main.py
@@ -11,21 +11,27 @@ HIDDEN_LAYER_NEURONS = [3, 5, 10, 15, 30, 50]
 if __name__ == '__main__':
     x = np.linspace(RANGE_START, RANGE_END, POINTS)
     size = len(x)
-    y1 = 2 * x * np.sin(x)
+
+    # CHANGE THIS CODE TO TEST ANOTHER TRAIN TYPE OR FUNCTION
+    y1 = 2 * pow(x, 1 / 3) * np.sin(x / 10) * np.cos(3 * x)
+    # y1 = 2 * x * np.sin(x)
+    train_type = nl.net.train.train_gdx
+    ##########################################################
+
     y = y1 / np.abs(max(y1) - min(y1))
     inputs = x.reshape(size, 1)
     targets = y.reshape(size, 1)
 
-    net = nl.net.newff([[RANGE_START, RANGE_END]], [10, 1])
-    net.trainf = nl.net.train.train_gdx
-    error = net.train(inputs, targets, epochs=1000, show=None, goal=0.00)
-    prediction = net.sim(inputs)
-    difference = mean_squared_error(targets, prediction)
-    print(round(difference, 4))
-    print(difference)
-
-    x2 = np.linspace(RANGE_START, RANGE_END, POINTS)
-    y2 = net.sim(x2.reshape(x2.size, 1)).reshape(x2.size)
-    plt.plot(x2, y2, '-', x, y, '-')
-    plt.legend(['learning result', 'real value'])
-    plt.show()
+    print("Type: ", train_type)
+    for neuron in HIDDEN_LAYER_NEURONS:
+        net = nl.net.newff([[RANGE_START, RANGE_END]], [neuron, 1])
+        net.trainf = train_type
+        error = net.train(inputs, targets, epochs=1000, show=None, goal=0.00)
+        prediction = net.sim(inputs)
+        difference = mean_squared_error(targets, prediction)
+        print("Hidden layer neurons: ", neuron, " - ", round(difference, 4))
+        y2 = net.sim(x.reshape(x.size, 1)).reshape(x.size)
+        plt.plot(x, y2, '-', x, y, '-')
+        plt.legend(['learning result', 'real value'])
+        plt.title("Function 2, " + str(train_type) + ", " + str(neuron) + " neurons in hidden layer")
+        plt.show()
