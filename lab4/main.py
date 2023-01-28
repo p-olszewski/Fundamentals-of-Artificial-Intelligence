3285e0ec5b947c696fcbd2cebf4ccecc27f80766
p-olszewski <przemo1297@gmail.com>
2023-01-28 23:27:57 +0100

    LAB4 count mean_squared_error, fix plot, code cleanup

--- lab4/main.py
+++ lab4/main.py
@@ -1,35 +1,31 @@
 import neurolab as nl
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 RANGE_START = 0
 RANGE_END = 9
-
-
-def low_amplitude_function():
-    y = 2 * x * np.sin(x)
-
+POINTS = 50
+HIDDEN_LAYER_NEURONS = [3, 5, 10, 15, 30, 50]
 
 if __name__ == '__main__':
-    x = np.linspace(-7, 7, 30)
-    y1 = 2 * x * np.sin(x)
-    wsp = np.abs(max(y1) - min(y1))
-    y = y1 / wsp
+    x = np.linspace(RANGE_START, RANGE_END, POINTS)
     size = len(x)
-    print(x)
-    inp = x.reshape(size, 1)
-    print(inp)
-    tar = y.reshape(size, 1)
+    y1 = 2 * x * np.sin(x)
+    y = y1 / np.abs(max(y1) - min(y1))
+    inputs = x.reshape(size, 1)
+    targets = y.reshape(size, 1)
 
-    net = nl.net.newff([[-7, 7]], [10, 1])
+    net = nl.net.newff([[RANGE_START, RANGE_END]], [10, 1])
     net.trainf = nl.net.train.train_gdx
-    error = net.train(inp, tar, epochs=1000, show=100, goal=0.00)
+    error = net.train(inputs, targets, epochs=1000, show=None, goal=0.00)
+    prediction = net.sim(inputs)
+    difference = mean_squared_error(targets, prediction)
+    print(round(difference, 4))
+    print(difference)
 
-    out = net.sim(inp)
-
-    x2 = np.linspace(-6.0, 6.0, 150)
+    x2 = np.linspace(RANGE_START, RANGE_END, POINTS)
     y2 = net.sim(x2.reshape(x2.size, 1)).reshape(x2.size)
-    y3 = out.reshape(size)
-    plt.plot(x2, y2, '-', x, y, '.', x, y3, 'p')
-    plt.legend(['wynik uczenia', 'wartosc rzeczywista'])
+    plt.plot(x2, y2, '-', x, y, '-')
+    plt.legend(['learning result', 'real value'])
     plt.show()
