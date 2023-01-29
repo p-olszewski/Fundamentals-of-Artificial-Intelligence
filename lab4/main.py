1f80ffe31e7fdc4d1018cf8ce2fb28dd6a97a791
p-olszewski <przemo1297@gmail.com>
2023-01-29 01:29:44 +0100

    LAB4 rename result variable

--- lab4/main.py
+++ lab4/main.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
         net.trainf = train_type  # set the train type
         error = net.train(inputs, targets, epochs=1000, show=None, goal=0.00)  # train the network
         prediction = net.sim(inputs)   # predict outputs for x values
-        difference = mean_squared_error(targets, prediction)  # calculate mean squared error
-        print("Hidden layer neurons: ", neuron, " - ", round(difference, 4))
+        result = mean_squared_error(targets, prediction)  # calculate mean squared error
+        print("Hidden layer neurons: ", neuron, " - ", round(result, 4))
         y2 = net.sim(x.reshape(x.size, 1)).reshape(x.size)  # get predicted y values
         plt.plot(x, y2, '-', x, y, '-')  # plot the predicted and real y values
         plt.legend(['learning result', 'real value'])
