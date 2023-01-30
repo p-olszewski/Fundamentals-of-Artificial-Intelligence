57745124012489a69e5bee472abe03f0efc3f64b
p-olszewski <przemo1297@gmail.com>
2023-01-30 19:57:38 +0100

    LAB5 add membership function

--- lab5/main.py
+++ lab5/main.py
@@ -5,5 +5,11 @@ import numpy as np
 import pandas as pd
 
 
+def membership_function(binary):
+    decimal = np.packbits(binary)
+    result = 0.2 * np.sqrt(decimal) + 2.0 * np.sin(2.0 * np.pi * 0.02 * decimal) + 5.0
+    return result
+
+
 if __name__ == '__main__':
     print("lab5")
