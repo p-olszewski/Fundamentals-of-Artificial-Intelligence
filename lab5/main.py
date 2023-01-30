06eba284b38e7dd35556e5c0842e568830732617
p-olszewski <przemo1297@gmail.com>
2023-01-30 21:08:29 +0100

    LAB5 add roulette function

--- lab5/main.py
+++ lab5/main.py
@@ -9,5 +9,9 @@ def adaptation_function(chromosomes):
     return 0.2 * np.sqrt(np.packbits(chromosomes)) + 2.0 * np.sin(2.0 * np.pi * 0.02 * np.packbits(chromosomes)) + 5.0
 
 
+def get_roulette_wheel(population):
+    return np.add.accumulate((adaptation_function(population) / sum(adaptation_function(population))) * 100)
+
+
 if __name__ == '__main__':
     print("lab5")
